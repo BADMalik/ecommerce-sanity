511a635630e2b8b1007ab0a6f60f022eddd10126
BADMalik <malikzaman133@gmail.com>
2023-02-26 21:46:36 +0500

    Completed

---
+++ ecommerce-sanity/ecommerce-stripe-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '041k413a',
+    dataset: 'production'
+  }
+})
---
+++ ecommerce-sanity/lib/client.js
@@ -0,0 +1,14 @@
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+const config = {
+  projectId: process.env.NEXT_PUBLIC_APP_SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2023-02-24",
+  useCdn: false,
+  token: process.env.NEXT_PUBLIC_APP_SANITY_TOKEN,
+};
+
+export const client = createClient(config);
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
+// console.log(client, builder, urlFor, "hi");
